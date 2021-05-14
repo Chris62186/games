915d5f1d9093239d5e36d63342f9214b23966859
Chris62186 <81889195+Chris62186@users.noreply.github.com>
2021-05-13 22:29:32 -0400

    Triggers and Counter Added
    Added space bar as a trigger to start/roll as well as a counter

--- gamescollection/Dice/index.js
+++ gamescollection/Dice/index.js
@@ -1,5 +1,8 @@
 var randomNumber1;
 var randomNumber2;
+var gameActive = false;
+var p1Wins = 0;
+var p2Wins = 0;
 
 diceP1 = randomizeDice();
 diceP2 = randomizeDice();
@@ -7,8 +10,22 @@ diceP2 = randomizeDice();
 document.querySelector(".img1").setAttribute("src", "images/dice" + diceP1 + ".png");
 document.querySelector(".img2").setAttribute("src", "images/dice" + diceP2 + ".png");
 
-determineWinner(diceP1, diceP2);
+if (!gameActive) {
+  document.querySelector("h1").innerHTML = "Press Space to Roll";
+}
 
+document.addEventListener("keydown", function() {
+
+  gameActive = true;
+
+  diceP1 = randomizeDice();
+  diceP2 = randomizeDice();
+
+  document.querySelector(".img1").setAttribute("src", "images/dice" + diceP1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + diceP2 + ".png");
+
+  determineWinner(diceP1, diceP2);
+})
 
 
 //FUNCTIONS
@@ -19,10 +36,17 @@ function randomizeDice () {
 
 function determineWinner(p1Value, p2Value) {
   if (p1Value > p2Value) {
+    p1Wins++;
+    console.log(p1Wins);
     document.querySelector("h1").innerHTML = "🏆 Player One Wins";
+    document.querySelector(".p1").innerHTML = p1Wins;
+
   }
   else if (p1Value < p2Value) {
+    p2Wins++;
+    console.log(p2Wins);
     document.querySelector("h1").innerHTML = "Player Two Wins 🏆";
+    document.querySelector(".p2").innerHTML = p2Wins;
   }
   else {
     document.querySelector("h1").innerHTML = "It's a tie!";
